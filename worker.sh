9af20443e986ed3367f07218b282a7f04371f6d8
error-try-again <yane.clarke@gmail.com>
2023-05-22 11:57:38 +1000

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Check if the Python script is running and kill it
+# Check if the Python script is running and terminate it
 if [ -f /tmp/chat.pid ]; then
     kill $(cat /tmp/chat.pid) 2>/dev/null
     rm /tmp/chat.pid
 fi
 
-# Starting the chat.py script
+# Start the chat.py script
 python3 ~/.local/bin/chat.py &
 
-# Send the command-line arguments to in_fifo
+# Send the command-line arguments to the input FIFO (in_fifo)
 for arg in "$@"; do
     echo "$arg" > /tmp/in_fifo
 done
 
 sleep 3
 
-# Read the output from out_fifo
+# Read the output from the output FIFO (out_fifo)
 while IFS= read -r line; do
+    # Cleanse the line by converting encoding to UTF-8 and remove invalid characters
     line=$(echo "$line" | iconv -f utf-8 -t utf-8 -c)
-    notify-send "$line"
+
+    # Display the line as a notification and append it to the chat log
+    notify-send "$line" && echo "$line" >> /tmp/chat.log
 done < /tmp/out_fifo
